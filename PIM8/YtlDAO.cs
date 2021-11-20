179b662b40130d8d59c017c1d734cb80da43a4cb
MauroFelipe <mauro.felipe.oc@gmail.com>
2021-11-20 17:35:50 -0300

    YdlDAO

---
+++ PIM8/YtlDAO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIM8
+{
+    class YtlDAO
+    {
+        static public bool Exclua(Pessoa pessoa)
+        {
+            return true;
+        }
+
+        static public bool Insira(Pessoa pessoa)
+        {
+            SqlConnection conexao = new SqlConnection(@"Server= localhost\SQLEXPRESS; Database= dbPIM8; Integrated Security=True;");
+            string strSQL = "INSERT INTO TESTE (NOME) VALUES(@NOME)";
+            SqlCommand comando = new SqlCommand(strSQL, conexao); ;
+            SqlDataAdapter da;
+            SqlDataReader dr;
+
+            comando.Parameters.AddWithValue("@NOME", pessoa.nome);
+            try
+            {
+                conexao.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            return true;
+        }
+
+        static public bool Altere(Pessoa pessoa)
+        {
+            return true;
+        }
+
+        static public Pessoa Consulte(long cpf)
+        {
+            Pessoa p = new Pessoa();
+            return p;
+        }
+    }
+}
