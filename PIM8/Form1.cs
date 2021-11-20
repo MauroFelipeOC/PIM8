81709bf9b87fc7bafb96049a1616b9107aff0e1c
MauroFelipe <mauro.felipe.oc@gmail.com>
2021-11-20 17:08:48 -0300

    Implementa PessoaDAO.Inserir()

--- PIM8/Form1.cs
+++ PIM8/Form1.cs
@@ -14,11 +14,30 @@ namespace PIM8
     {
         public Form1()
         {
-            Pessoa p = new Pessoa();
-            p.ID = 4;
-            p.nome = "Fulano";
-            p.cpf = 046;            
             InitializeComponent();
+
+            Pessoa p = new Pessoa();
+            //p.ID = 4;
+            p.nome = "Fulano";
+            p.cpf = 046;
+            //p.endereco
+            p.endereco = new Endereco();
+            //p.endereco.ID = 4;
+            p.endereco.logradouro = "Rua";
+            p.endereco.numero = 1;
+            p.endereco.cep = 1234;
+            p.endereco.bairro = "Ipiranga";
+            p.endereco.cidade = "Paris";
+            p.endereco.estado = "AP";
+            //tipoTelefone
+            Telefone telefone = new Telefone();
+            telefone.numero = 32129999;
+            telefone.ddd = 86;
+            TipoTelefone tipoTelefone = new TipoTelefone();
+            tipoTelefone.tipo = "Celular";
+            telefone.tipoTelefone = tipoTelefone;
+            p.telefones.Add(telefone);
+            
             PessoaDAO.Insira(p);
             
         }
--- PIM8/PessoaDAO.cs
+++ PIM8/PessoaDAO.cs
@@ -17,42 +17,47 @@ namespace PIM8
         static public bool Insira(Pessoa pessoa)
         {
             SqlConnection conexao = new SqlConnection(@"Server= localhost\SQLEXPRESS; Database= TestePIM; Integrated Security=True;");
-            string strSQL = "INSERT INTO PESSOA (NOME, CPF, ENDERECO) VALUES(@NOME, @CPF, @ENDERECO)";
-            SqlCommand cmdPessoa = new SqlCommand(strSQL, conexao);
-            SqlDataAdapter da;
-            SqlDataReader dr;
-
-            //comando.Parameters.AddWithValue("@ID", pessoa.ID);
-            cmdPessoa.Parameters.AddWithValue("@NOME", pessoa.nome);
-            cmdPessoa.Parameters.AddWithValue("@CPF", pessoa.cpf);
-            cmdPessoa.Parameters.AddWithValue("@ENDERECO", 4);
+            string strSQL;
 
             strSQL = "INSERT INTO ENDERECO (LOGRADOURO, NUMERO, CEP, BAIRRO, CIDADE, UF) VALUES(@LOGRADOURO, @NUMERO, @CEP, @BAIRRO, @CIDADE, @UF)";
             SqlCommand cmdEndereco = new SqlCommand(strSQL, conexao);
             cmdEndereco.Parameters.AddWithValue("@LOGRADOURO", pessoa.endereco.logradouro);
             cmdEndereco.Parameters.AddWithValue("@NUMERO", pessoa.endereco.numero);
             cmdEndereco.Parameters.AddWithValue("@CEP", pessoa.endereco.cep);
-            cmdEndereco.Parameters.AddWithValue("@BAIRRO", pessoa.endereco.cidade);
+            cmdEndereco.Parameters.AddWithValue("@BAIRRO", pessoa.endereco.bairro);
+            cmdEndereco.Parameters.AddWithValue("@CIDADE", pessoa.endereco.cidade);
             cmdEndereco.Parameters.AddWithValue("@UF", pessoa.endereco.estado);
+
+            strSQL = "INSERT INTO PESSOA (NOME, CPF, ENDERECO) VALUES(@NOME, @CPF, @ENDERECO)";
+            SqlCommand cmdPessoa = new SqlCommand(strSQL, conexao);
+            //comando.Parameters.AddWithValue("@ID", pessoa.ID);
+            cmdPessoa.Parameters.AddWithValue("@NOME", pessoa.nome);
+            cmdPessoa.Parameters.AddWithValue("@CPF", pessoa.cpf);
+            cmdPessoa.Parameters.AddWithValue("@ENDERECO", 4);
+
+            /*strSQL = "INSERT INTO TELEFONE_TIPO (TIPO) VALUES(@TIPO)";
+            SqlCommand cmdTelefoneTipo = new SqlCommand(strSQL, conexao);
+            cmdTelefoneTipo.Parameters.AddWithValue("@TIPO", 1);*/
+            
+
+            strSQL = "INSERT INTO TELEFONE (NUMERO, DDD, TIPO) VALUES(@NUMERO, @DDD, @TIPO)";
+            SqlCommand cmdTelefone = new SqlCommand(strSQL, conexao);
+            cmdTelefone.Parameters.AddWithValue("@NUMERO", 32121234);
+            cmdTelefone.Parameters.AddWithValue("@DDD", pessoa.telefones[0].ddd);
+            cmdTelefone.Parameters.AddWithValue("@TIPO", 1 /*pessoa.telefones[0].tipoTelefone*/);
 
             strSQL = "INSERT INTO PESSOA_TELEFONE (ID_PESSOA, ID_TELEFONE) VALUES(@ID_PESSOA, @ID_TELEFONE)";
             SqlCommand cmdPessoaTelefone = new SqlCommand(strSQL, conexao);
-            cmdPessoaTelefone.Parameters.AddWithValue("@ID_PESSOA", pessoa.ID);
-            cmdPessoaTelefone.Parameters.AddWithValue("@ID_TELEFONE", pessoa.telefones[0].ID);
-
-            strSQL = "INSERT INTO PESSOA_TELEFONE (NUMERO, DDD, TIPO) VALUES(@NUMERO, @DDD, @TIPO)";
-            SqlCommand cmdTelefone = new SqlCommand(strSQL, conexao);
-            cmdTelefone.Parameters.AddWithValue("@NUMERO", pessoa.ID);
-            cmdTelefone.Parameters.AddWithValue("@DDD", pessoa.telefones[0].ID);
-            cmdTelefone.Parameters.AddWithValue("@TIPO", pessoa.telefones[0].ID);
+            cmdPessoaTelefone.Parameters.AddWithValue("@ID_PESSOA", 4 /*pessoa.ID*/);
+            cmdPessoaTelefone.Parameters.AddWithValue("@ID_TELEFONE", 4 /*pessoa.telefones[0].ID*/);            
 
             try
             {
                 conexao.Open();
+                cmdEndereco.ExecuteNonQuery();
                 cmdPessoa.ExecuteNonQuery();
-                //cmdEndereco.ExecuteNonQuery();
-                //cmdPessoaTelefone.ExecuteNonQuery();
-                //cmdTelefone.ExecuteNonQuery();
+                cmdTelefone.ExecuteNonQuery();
+                cmdPessoaTelefone.ExecuteNonQuery();
             }
             catch (Exception)
             {
